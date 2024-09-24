168ce0334f597d878f5bcb1233781f1c032ccbde
umiko <junx026@gmail.com>
2024-09-24 17:13:43 +0800

    modifiedAplus

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import os
 import geopandas as gp
 import pandas as pd
 import shapely as sp
 import numpy as np
 import pyproj
-from preprocessing import crs,roundLocation
-from preprocessing import preprocessSHP,plotObject
-from preprocessing import loadPOI,loadLOI
+from preprocess import crs, roundLocation
+import preprocess
 
 
 def Ax(start: str, end: str, POI: gp.GeoDataFrame) -> np.ndarray[str]:
@@ -61,6 +61,7 @@ def Ax(start: str, end: str, POI: gp.GeoDataFrame) -> np.ndarray[str]:
         path.append(tempPOI.loc[path[-1], 'target'])
     return np.array(path).astype(str)
 
+
 def modifiedAx(start: str, end: str, POI: gp.GeoDataFrame) -> list[str]:
     # def gx(now):
     #     origin = now
@@ -104,11 +105,16 @@ def modifiedAx(start: str, end: str, POI: gp.GeoDataFrame) -> list[str]:
         return False
 
     iters = 0
-
     while not search(NEXT):
         iters += 1
-        _min = np.argmin(OPEN.values())
-        NEXT = list(OPEN.keys())[_min]
+        NEST = {tag: OPEN[tag] - hx(tag) for tag in OPEN.keys() if OPEN[tag] < fm}
+        if NEST:
+            _min = np.argmin(NEST.values())
+            NEXT = list(NEST.keys())[_min]
+        else:
+            _min = np.argmin(OPEN.values())
+            NEXT = list(OPEN.keys())[_min]
+            fm = OPEN[NEXT]
         print(f'\rSTART:{start} END:{end} NEXT:{NEXT} iters:{iters}', end='')
     print('\r', end='')
     path = [end]
@@ -116,33 +122,36 @@ def modifiedAx(start: str, end: str, POI: gp.GeoDataFrame) -> list[str]:
         path.append(tempPOI.loc[end, 'target'])
     return path
 
+
 def locate(lng: float, lat: float, POI: gp.GeoDataFrame) -> str:
     if lat < 180:
         transformer = pyproj.Transformer.from_crs(4326, crs)
-        lng, lat = transformer.transform( lng, lat)
-    lng, lat = roundLocation(lng, lat,toStr=False)
-    absDist = np.abs(POI['x']-lng)+np.abs(POI['y']-lat)
+        lng, lat = transformer.transform(lng, lat)
+    lng, lat = roundLocation(lng, lat, toStr=False)
+    absDist = np.abs(POI['x'] - lng) + np.abs(POI['y'] - lat)
     loc = POI.iloc[np.argmin(absDist)]
     return loc
 
-def path2Polyline(poiList,POI)->sp.Geometry:
-    poiList=POI.loc[poiList]
-    return sp.linestrings(poiList[['rx','ry']].to_numpy())
+
+def path2Polyline(poiList, POI) -> sp.Geometry:
+    poiList = POI.loc[poiList]
+    return sp.linestrings(poiList[['rx', 'ry']].to_numpy())
+
 
 if __name__ == '__main__':
-    from matplotlib import pyplot as plt
-    POI = loadPOI()
-    # LOI = loadLOI() # not required
-    # plotObject(*LOI.geometry,color='black',alpha=0.2,show=False)
-    print()
-    ref=[54493.5,20736.3]
-    start = locate(ref[0],ref[1],POI).name
-    rands = (np.random.rand(100,2)-0.5)*1000
-    end = [locate(ref[0]+r[0],ref[1]+r[1],POI).name for r in rands]
+    '''include the shp to the library'''
+    preprocess.preprocessSHP(*['shp\\' + f for f in os.listdir('shp') if f.endswith('.shp')])
+    '''load node POI file'''
+    POI = preprocess.loadPOI()
+    '''locate the point(s)'''
+    ref = [54493.5, 20736.3]
+    start = locate(ref[0], ref[1], POI).name
+    rands = (np.random.rand(100, 2) - 0.5) * 1000
+    end = [locate(ref[0] + r[0], ref[1] + r[1], POI).name for r in rands]
+    '''find the path'''
     pLs = []
     for ed in end:
-        pLine=Ax(start,ed,POI)
-        pLs.append(path2Polyline(pLine,POI))
-    plotObject(*pLs,color='red')
-    gp.GeoDataFrame(pd.DataFrame(range(len(pLs)),columns=['0']),geometry=pLs,crs=crs).to_file('roadBeijing.shp')
-
+        pLine = Ax(start, ed, POI)
+        # pLine = modifiedAx(start, ed, POI)
+        pLs.append(path2Polyline(pLine, POI))
+    preprocess.plotObject(*pLs, color='red')
